993fcfa5bba637c87da0f4fea4f9f6881abd9c5c
anita <jingtang.87@gmail.com>
2013-12-08 22:16:08 +0800

    update tab effect and about page

--- website/app01/views.py
+++ website/app01/views.py
@@ -10,32 +10,32 @@ import datetime,os,json,time
 
 def index(request):
 
-	return render_to_response('index.html')
+	return render_to_response('index.html',{'tabname':'homepage'})
 def about(request):
-	return render_to_response('about.html')
+	return render_to_response('about.html',{'tabname':'about'})
 
 def courses(request):
-	return render_to_response('courses.html')
+	return render_to_response('courses.html',{'tabname':'courses'})
 
 def graduate(request):
 	student_list = models.graduates.objects.all()
-        return render_to_response('graduate.html',{'student_list':student_list})
+        return render_to_response('graduate.html',{'student_list':student_list,'tabname':'graduate'})
                                                  
 def enroll(request):
 	question_list = models.questions.objects.all()
-	return render_to_response('enroll.html',{'question_list': sorted(question_list) }) 
+	return render_to_response('enroll.html',{'question_list': sorted(question_list) ,'tabname':'enroll'}) 
 
 def newClass(request):
 	new_classes = models.new_classes.objects.all()
-	return render_to_response('newClass.html', {'new_classes': new_classes})
+	return render_to_response('newClass.html', {'new_classes': new_classes,'tabname':'newClass'})
 def onLineCourse(request):
-        return render_to_response('onLineCourse.html')
+        return render_to_response('onLineCourse.html',{'tabname':'onLineCourse'})
 def bbs(request):
-        return render_to_response('bbs.html')
+        return render_to_response('bbs.html',{'tabname':'bbs'})
 def news(request):
 	news_list = models.news.objects.all()
-        return render_to_response('news.html', {'news_list': news_list})
+        return render_to_response('news.html', {'news_list': news_list,'tabname':'news'})
 def contact(request):
-        return render_to_response('contact.html')
+        return render_to_response('contact.html',{'tabname':'contact'})
 
 
